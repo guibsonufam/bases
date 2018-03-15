8889539797f97d390cf36341ae047689743d40fa
guibson <guibsonsfc44@gmail.com>
2018-03-15 16:17:50 -0400

    Atualização da base de usuários contra-lula

--- eleicao/prolula/prolula/DOCS_GERAL/prolula.sql
+++ eleicao/prolula/prolula/DOCS_GERAL/prolula.sql
@@ -1,10 +1,10 @@
-/* Contas suspeitas
+/* Contas suspeitas*/
 select user.id_str, user.screen_name, user.created_at, text, retweeted_status.user.screen_name
 from prolula
-where user.id_str='955968218253930497';
-*/
+where user.id_str='956291395119124481';
 
-/* Usuários com alto degree*/
+
+/* Usuários com alto degree
 select created_at, user.id_str, user.screen_name, text, retweeted_status.user.screen_name, retweeted_status.user.id_str, retweet_count, favorite_count
 from prolula
-where user.id_str='796180188946112512';
+where user.id_str='796180188946112512';*/
