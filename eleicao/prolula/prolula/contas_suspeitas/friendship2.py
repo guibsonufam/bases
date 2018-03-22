1d2de9430bf9b8e554419328874afbf3a0533b91
guibson <guibsonsfc44@gmail.com>
2018-03-22 16:25:45 -0400

    Adição das contas suspeitas (contra-lula) e Análise das contas suspeitas (pró-lula)

--- eleicao/prolula/prolula/contas_suspeitas/friendship2.py
+++ eleicao/prolula/prolula/contas_suspeitas/friendship2.py
@@ -1,65 +1,39 @@
 import tweepy
-import csv
-import time
+import sys
 import os
-import json
-import sys
-import sys
+import time
 
-# -*- encoding: utf-8 -*-
-consumer_key="8OKW5ik3n8DqT4npsBMQ5zZQI"
-consumer_secret="RxysoCbyEhzLWyyPmgVgFjmiEJw4fvUo2pd0i2fmfQYKpgTpQj"
-acess_token="191712148-a8fuYrl7R7TDlYGunaOArmD2qF0GvAxGiINV2lbc"
-acess_token_secret="AVr5CSGBXfoCpTiSmgYAjInqd1a9PYvHm7p9k9aHCYpoS"
+consumer_key        = "twuQZxuLID8wioORp7nUCxPWu"
+consumer_secret     = "ebQoltU1d3RnhaQdpOwHeIQ3oH7GmvfkJYo2NeilrSk3KaKE4b"
+access_token        = "123043086-L89ej8XQCePoCGC3vWT3qKwTWtiTGtlUxAJNyz7Y"
+access_token_secret = "rje1kNUBr6aGgAToKZUOjjj2nUBUVllvSuqIJ3rFxiVJh"
 
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(acess_token,acess_token_secret)
-userID_target = '76049312'
-api=tweepy.API(auth, wait_on_rate_limit=True)
 
-row_segue=[]
-row_naoSegue=[]
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-arq2 = open("seguem.txt", "w",encoding= 'utf-8')
-arq3 = open("nao_seguem.txt", "w",encoding= 'utf-8')
-arq4 = open("error_exception","w",encoding= 'utf-8')
-arq = open('ids.txt', 'r')
+api = tweepy.API(auth)
+
+
+arq = open('contas_suspeitas_prolula.txt', 'r')
 texto = arq.readlines()
 
-for linha in texto :
-    try:    
-        print(linha)    
-        a = (api.show_friendship(source_id=linha, target_id=userID_target))[0].following
-        row_segue = (api.show_friendship(source_id=linha, target_id=userID_target))
-
-        if a==True:
-            row_segue = str(row_segue[0].id_str), str(row_segue[1].id_str)
-            #print(row)        
-            arq2.write(str(row_segue)+'\n')
-            
-       
-        elif a==False:
-            #print("nao segue")
-            row_naoSegue = str(row_segue[0].id_str), str(row_segue[1].id_str)
-            arq3.write(str(row_naoSegue)+'\n')
-
-    
-    except tweepy.TweepError as e:
-        if e.api_code == 163:
-            print("achou erro 163")         
-            arq4.write(linha + e.reason +'\n')        
-        else:        
-            print("wait 15 minutes...")
-            arq4.write(linha + e.reason +'\n')
-            #+str(count)        
-            time.sleep(60 * 15)
-            #cont=0
-        continue
-    except StopIteration:
-        print("Acabou a interacao")
-        #print (count)
-        break
+for linha in texto:
+	arq2 = open("contas_suspeitas_text_" + linha + ".txt", "w",encoding= 'utf-8')
+	stuff = tweepy.Cursor(api.user_timeline, user_id = linha, count = 10, include_rts = True).items()
+	#for linha in texto:
+	cont=0
+	for status in stuff:
+		try:
+			arq2.write(str(status.created_at) + '\n')
+			arq2.write(status.text + '\n')
+			#arq2.write('\n')
+			#print (status.text)
+			cont = cont + 1
+			if(cont==10):
+				break
+		except Exception as e:
+			print("wait 15 minutes")
+			time.sleep(60*15)
 
 arq.close()
-
-#print(a[0].following, a[1].following)
